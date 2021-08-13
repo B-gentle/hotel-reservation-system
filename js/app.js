20643c54ac5ac5cb662003fdd3db6dae3581971a
BeeTech <brighteyo16@gmail.com>
2021-08-13 23:09:49 +0100

    added the function to control form prices and added new fonts

--- js/app.js
+++ js/app.js
@@ -4,35 +4,39 @@ $(function() {
 	});
 });
 
-let x = document.querySelector('#checkInDate');
-x.addEventListener('input', dueDate);
+let checkInDate = document.querySelector('#checkInDate');
+let duration = document.querySelector('#duration');
+let checkOutDate = document.querySelector('#checkOutDate');
+console.log(duration);
+duration.addEventListener('input', dueDate);
 
 function dueDate() {
-  let y = x.value;
-  alert(parseInt(y) + 3);
-  let c = document.createElement(input);
-//   c.setAttribute(type)
+  let durationValue = duration.value;
+  let checkInDateValue = checkInDate.value;
+
+  if (durationValue  && checkInDateValue){
+    let outputDate = parseFloat(checkInDateValue + durationValue);
+    checkOutDate.value = outputDate;
+    alert(durationValue);
+  //   c.setAttribute(type)
+  }else{
+    alert("enter duration");
+  };
+  
 };
 
 let selectRoom = document.getElementById("select-room");
 let chooseMeal = document.querySelector('#chooseMeal');
 let roomPrice = document.getElementById('roomPrice');
 let foodPrice = document.getElementById("foodPrice");
-chooseMeal.addEventListener('change', pricetag)
+chooseMeal.addEventListener('change', pricetag);
 selectRoom.addEventListener('change', pricet);
   
-function pricetag(price, select) {
-  return foodPrice.value  = chooseMeal.value;
-}
+function pricetag() {
+ foodPrice.value  = chooseMeal.value;
+ }
 
 function pricet() {
-  return roomPrice.value = selectRoom.value;
+   roomPrice.value = selectRoom.value;
 }
-
-// function pricetag(price, select) {
-//   return price.value = select.value;
-// }
-
-// pricetag(roomPrice, selectRoom);
-// pricetag(foodPrice, chooseMeal);
 
